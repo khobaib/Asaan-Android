fc6ea30439dca7411ac2863c223de4e8ed68343a
khobaib <khobaib@gmail.com>
2014-10-19 20:03:24 +0600

    core reformatting.

--- src/com/techfiesta/asaan/fragment/SignupCardinfoFragment.java
+++ src/com/techfiesta/asaan/fragment/SignupCardinfoFragment.java
@@ -1,15 +1,11 @@
 package com.techfiesta.asaan.fragment;
 
 import java.util.ArrayList;
-import java.util.ListIterator;
 
 import android.app.Activity;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,8 +28,8 @@ import com.techfiesta.asaan.R;
 import com.techfiesta.asaan.adapter.NothingSelectedSpinnerAdapter;
 import com.techfiesta.asaan.communication.Communicator;
 
-public class SignupCardinfoFragment extends Fragment{
-	
+public class SignupCardinfoFragment extends Fragment {
+
 	public static final String PUBLISHABLE_KEY = "pk_test_hlpADPUOWaxn6uN0aATgLivW";
 	EditText CardNumber;
 	EditText CVC;
@@ -42,9 +38,9 @@ public class SignupCardinfoFragment extends Fragment{
 	Button SaveCard;
 	EditText DefaultTip;
 	Button SaveTip;
-	
+
 	ImageView NEXT2;
-	
+
 	int expMonth, expYear;
 	String cardNumber;
 	String cardCVC;
@@ -53,14 +49,15 @@ public class SignupCardinfoFragment extends Fragment{
 	public SignupCardinfoFragment() {
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		return inflater.inflate(R.layout.sign_up_frag_2, container, false);
 	}
-	
+
 	@Override
-	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
 		Activity mActivity = getActivity();
@@ -72,168 +69,163 @@ public class SignupCardinfoFragment extends Fragment{
 		DefaultTip = (EditText) mActivity.findViewById(R.id.etDefaultTipSignUp);
 		SaveTip = (Button) mActivity.findViewById(R.id.btnSaveTipInfoSu);
 		comm = (Communicator) getActivity();
-		
+
 		updateMonth_YearSpinners();
-		
+
 		Month.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				if(position>1){
+				if (position > 1) {
 					expMonth = Integer.parseInt(parent.getSelectedItem().toString());
 				}
-				
+
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
+
 		Year.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				if(position>1){
+				if (position > 1) {
 					expYear = Integer.parseInt(parent.getSelectedItem().toString());
 				}
-				
+
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
-		
+
 		SaveCard.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				cardNumber = CardNumber.getText().toString();
 				cardCVC = CVC.getText().toString();
 				saveCreditCard();
-				
+
 			}
 		});
-		
+
 		SaveTip.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				String tip = DefaultTip.getText().toString();
 				System.out.println(tip);
-				//save it to respective parse class when this field is added
+				// save it to respective parse class when this field is added
 			}
 		});
 		NEXT2 = (ImageView) mActivity.findViewById(R.id.ivForward);
 		NEXT2.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				comm.respond(2);
-				
+
 			}
 		});
-		
+
 	}
-	
-	 public void saveCreditCard() {
 
-	        Card card = new Card(
-	                cardNumber,
-	                expMonth,
-	                expYear,
-	                cardCVC);
+	public void saveCreditCard() {
 
-	        boolean validation = card.validateCard();
-	        if (validation) {
-	           // startProgress();
-	            new Stripe().createToken(
-	                    card,
-	                    PUBLISHABLE_KEY,
-	                    new TokenCallback() {
-	                    public void onSuccess(Token token) {
-	                    
-	                    	System.out.println(""+token.getId());
-	                    	 saveToken(token);
-	                        }
-	                    public void onError(Exception error) {
-	                            handleError(error.getLocalizedMessage());
-	                           // finishProgress();
-	                        }
-	                    });
-	        } else if (!card.validateNumber()) {
-	        	handleError("The card number that you entered is invalid");
-	        } else if (!card.validateExpiryDate()) {
-	        	handleError("The expiration date that you entered is invalid");
-	        } else if (!card.validateCVC()) {
-	        	handleError("The CVC code that you entered is invalid");
-	        } else {
-	        	handleError("The card details that you entered are invalid");
-	        }
-	    }
-	 
-	 private void saveToken(Token token){
-		 System.out.println(token.getCard().getFingerprint());
-		 Card tokCard = token.getCard();
-		 if(tokCard!=null){
-		 ParseObject strCard = new ParseObject("UserStripeCard");
-		// strCard.put("address_city", tokCard.getAddressCity());
-		// strCard.put("address_country", tokCard.getAddressCountry());
-		// strCard.put("address_line1", tokCard.getAddressLine1());
-		// strCard.put("address_line2", tokCard.getAddressLine2());
-		// strCard.put("address_state", tokCard.getAddressState());
-		// strCard.put("address_zip", tokCard.getAddressZip());
-		// strCard.put("brand", tokCard.getType());
-		 strCard.put("country", tokCard.getCountry());
-		 strCard.put("exp_month", tokCard.getExpMonth());
-		 strCard.put("exp_year", tokCard.getExpYear());
-		 strCard.put("fingerprint", tokCard.getFingerprint());
-		 //strCard.put("funding", null);
-		 strCard.put("last4", tokCard.getLast4());
-		// strCard.put("name", tokCard.getName());
-		// strCard.put("stripeCustomer", tokCard.get);
-		// strCard.put("cvc_check", tokCard.getCVC());
-		 if(ParseUser.getCurrentUser()!=null){
-			 strCard.put("user", ParseUser.getCurrentUser());
-		 }
-		 
-		 strCard.saveInBackground();
-		 }
-	 }
-	
-	
-	private void updateMonth_YearSpinners(){
-		ArrayList<String> monthList = new ArrayList<String>();
-		for(int i=1;i<=12;i++){
-			monthList.add(""+i);
+		Card card = new Card(cardNumber, expMonth, expYear, cardCVC);
+
+		boolean validation = card.validateCard();
+		if (validation) {
+			// startProgress();
+			new Stripe().createToken(card, PUBLISHABLE_KEY, new TokenCallback() {
+				public void onSuccess(Token token) {
+
+					System.out.println("" + token.getId());
+					saveToken(token);
+				}
+
+				public void onError(Exception error) {
+					handleError(error.getLocalizedMessage());
+					// finishProgress();
+				}
+			});
+		} else if (!card.validateNumber()) {
+			handleError("The card number that you entered is invalid");
+		} else if (!card.validateExpiryDate()) {
+			handleError("The expiration date that you entered is invalid");
+		} else if (!card.validateCVC()) {
+			handleError("The CVC code that you entered is invalid");
+		} else {
+			handleError("The card details that you entered are invalid");
 		}
-		ArrayAdapter<String> sMonthAdapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_textview,monthList);
-		Month.setAdapter(new NothingSelectedSpinnerAdapter(sMonthAdapter, R.layout.row_spinner_nothing_selected, getActivity()));
+	}
+
+	private void saveToken(Token token) {
+		System.out.println(token.getCard().getFingerprint());
+		Card tokCard = token.getCard();
+		if (tokCard != null) {
+			ParseObject strCard = new ParseObject("UserStripeCard");
+			// strCard.put("address_city", tokCard.getAddressCity());
+			// strCard.put("address_country", tokCard.getAddressCountry());
+			// strCard.put("address_line1", tokCard.getAddressLine1());
+			// strCard.put("address_line2", tokCard.getAddressLine2());
+			// strCard.put("address_state", tokCard.getAddressState());
+			// strCard.put("address_zip", tokCard.getAddressZip());
+			// strCard.put("brand", tokCard.getType());
+			strCard.put("country", tokCard.getCountry());
+			strCard.put("exp_month", tokCard.getExpMonth());
+			strCard.put("exp_year", tokCard.getExpYear());
+			strCard.put("fingerprint", tokCard.getFingerprint());
+			// strCard.put("funding", null);
+			strCard.put("last4", tokCard.getLast4());
+			// strCard.put("name", tokCard.getName());
+			// strCard.put("stripeCustomer", tokCard.get);
+			// strCard.put("cvc_check", tokCard.getCVC());
+			if (ParseUser.getCurrentUser() != null) {
+				strCard.put("user", ParseUser.getCurrentUser());
+			}
+
+			strCard.saveInBackground();
+		}
+	}
+
+	private void updateMonth_YearSpinners() {
+		ArrayList<String> monthList = new ArrayList<String>();
+		for (int i = 1; i <= 12; i++) {
+			monthList.add("" + i);
+		}
+		ArrayAdapter<String> sMonthAdapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_textview,
+				monthList);
+		Month.setAdapter(new NothingSelectedSpinnerAdapter(sMonthAdapter, R.layout.row_spinner_nothing_selected,
+				getActivity()));
 		sMonthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-	
+
 		ArrayList<String> yearList = new ArrayList<String>();
-		int thisYear = 2014; //here we will take current time year and replace with it
-		for(int i=1;i<=30;i++){
-			
-			yearList.add(""+thisYear);
+		int thisYear = 2014; // here we will take current time year and replace
+								// with it
+		for (int i = 1; i <= 30; i++) {
+
+			yearList.add("" + thisYear);
 			thisYear++;
 		}
-		ArrayAdapter<String> sYearAdapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_textview,yearList);
-		Year.setAdapter(new NothingSelectedSpinnerAdapter(sYearAdapter, R.layout.row_spinner_nothing_selected, getActivity()));
+		ArrayAdapter<String> sYearAdapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_textview, yearList);
+		Year.setAdapter(new NothingSelectedSpinnerAdapter(sYearAdapter, R.layout.row_spinner_nothing_selected,
+				getActivity()));
 		sYearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		
-		
+
 	}
-	
-	 private void handleError(String error) {
-	        DialogFragment fragment = ErrorDialogFragment.newInstance(R.string.validationErrors, error);
-	        fragment.show(getChildFragmentManager(), "error");
-	    }
+
+	private void handleError(String error) {
+		DialogFragment fragment = ErrorDialogFragment.newInstance(R.string.validationErrors, error);
+		fragment.show(getChildFragmentManager(), "error");
+	}
 
 }
--- src/com/techfiesta/asaan/fragment/SignupInvitecontactsFragment.java
+++ src/com/techfiesta/asaan/fragment/SignupInvitecontactsFragment.java
@@ -1,22 +1,21 @@
 package com.techfiesta.asaan.fragment;
 
-import com.techfiesta.asaan.R;
-
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SignupInvitecontactsFragment extends Fragment{
+import com.techfiesta.asaan.R;
+
+public class SignupInvitecontactsFragment extends Fragment {
 
 	public SignupInvitecontactsFragment() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		return inflater.inflate(R.layout.sign_up_frag_3, container, false);
 	}
