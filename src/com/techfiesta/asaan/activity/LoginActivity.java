00015692ea1f50ff316df7a79c078e2f64231e56
SBRishta <sbrishta.13@gmail.com>
2014-12-11 16:03:25 +0600

    Login Acitivity added

--- src/com/techfiesta/asaan/activity/LoginActivity.java
+++ src/com/techfiesta/asaan/activity/LoginActivity.java
@@ -1,12 +1,26 @@
 package com.techfiesta.asaan.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 import com.techfiesta.asaan.R;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
-
 public class LoginActivity extends Activity{
+	
+	EditText userEmail;
+	EditText passWord;
+	Button Login;
+	ParseUser currentUser;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -14,6 +28,44 @@ public class LoginActivity extends Activity{
 		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
 		getActionBar().hide();
 		setContentView(R.layout.activity_login_2);
+		
+		userEmail = (EditText) findViewById(R.id.et_email);
+		passWord = (EditText) findViewById(R.id.et_pass);
+		Login = (Button) findViewById(R.id.b_login);
+		Login.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				LoginUser();
+				
+			}
+		});
+	}
+	
+	private void launchActivity(Class<?> launchingClass) {
+		Intent intent = new Intent(this, launchingClass);
+		startActivity(intent);
+		finish();
+	}
+
+	private void LoginUser() {
+		String name = userEmail.getText().toString();
+		String password = passWord.getText().toString();
+		ParseUser.logInInBackground(name, password, new LogInCallback() {
+
+			@Override
+			public void done(ParseUser user, ParseException e) {
+				if (user != null) {
+
+					currentUser = ParseUser.getCurrentUser();
+					Log.d(">>", "login success" + currentUser.getEmail());
+					launchActivity(StoreListActivity.class);
+				} else {
+					Log.d(">>", "login failed");
+				}
+
+			}
+		});
 	}
 
 }
