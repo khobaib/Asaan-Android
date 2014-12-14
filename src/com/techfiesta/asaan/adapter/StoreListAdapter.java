40a74e8ba5675d42955583fb58a1b268fccf4dfb
khobaib <khobaib@gmail.com>
2014-12-14 11:03:01 +0600

    refactoring

--- src/com/techfiesta/asaan/adapter/StoreListAdapter.java
+++ src/com/techfiesta/asaan/adapter/StoreListAdapter.java
@@ -1,22 +1,13 @@
 package com.techfiesta.asaan.adapter;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-
-import javax.crypto.spec.IvParameterSpec;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteAbortException;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -34,16 +25,9 @@ import asaan.dao.DaoSession;
 import asaan.dao.ModItemDao;
 
 import com.asaan.server.com.asaan.server.endpoint.storeendpoint.model.Store;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseImageView;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.techfiesta.asaan.R;
 import com.techfiesta.asaan.activity.MenuActivity;
 import com.techfiesta.asaan.activity.MyCartActivity;
-import com.techfiesta.asaan.activity.PlaceOrderActivity;
 import com.techfiesta.asaan.lazylist.ImageLoader;
 import com.techfiesta.asaan.utility.AsaanUtility;
 
@@ -55,25 +39,26 @@ public class StoreListAdapter extends ArrayAdapter<Store> {
 	private ImageLoader imageLoader;
 
 	public StoreListAdapter(Context context, List<Store> stores) {
-		super(context, R.layout.restaurant_item_row, stores);
+		super(context, R.layout.row_restaurant_list, stores);
 		this.mContext = context;
 		this.storeList = stores;
 		this.mLocation = null;
 		imageLoader = new ImageLoader((Activity) context);
 	}
-   public void setLocation(Location location)
-   {
-	   this.mLocation=location;
-   }
+
+	public void setLocation(Location location) {
+		this.mLocation = location;
+	}
+
 	public StoreListAdapter(Context context, List<Store> stores, Location location) {
-		super(context, R.layout.restaurant_item_row, stores);
+		super(context, R.layout.row_restaurant_list, stores);
 		this.mContext = context;
 		this.storeList = stores;
 		this.mLocation = location;
 	}
 
 	private class ViewHolder {
-		//ParseImageView ivPhoto;
+		// ParseImageView ivPhoto;
 		ImageView ivPhoto;
 		TextView tvName;
 		TextView tvThrophy;
@@ -87,7 +72,7 @@ public class StoreListAdapter extends ArrayAdapter<Store> {
 
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
-		 ViewHolder holder = null;
+		ViewHolder holder = null;
 		LayoutInflater mInflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 
 		if (convertView == null) {
@@ -97,88 +82,81 @@ public class StoreListAdapter extends ArrayAdapter<Store> {
 			holder.tvName = (TextView) convertView.findViewById(R.id.tv_restaurant_name);
 			holder.tvThrophy = (TextView) convertView.findViewById(R.id.tv_first_trophy);
 			holder.tvSubType = (TextView) convertView.findViewById(R.id.tv_subtype);
-			holder.btnCall=(Button)convertView.findViewById(R.id.b_call);
-			holder.btnReserve=(Button)convertView.findViewById(R.id.b_reserve);
-			holder.btnMenu=(Button)convertView.findViewById(R.id.b_menu);
-			holder.btnorder=(Button)convertView.findViewById(R.id.b_online_order);
+			holder.btnCall = (Button) convertView.findViewById(R.id.b_call);
+			holder.btnReserve = (Button) convertView.findViewById(R.id.b_reserve);
+			holder.btnMenu = (Button) convertView.findViewById(R.id.b_menu);
+			holder.btnorder = (Button) convertView.findViewById(R.id.b_online_order);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
 		Store store = storeList.get(position);
-		Log.e("url",store.getBackgroundImageUrl());
+		Log.e("url", "image url for id " + store.getId() + " = "
+				+ ((store.getBackgroundImageUrl() == null) ? "null" : store.getBackgroundImageUrl()));
 		imageLoader.DisplayImage(store.getBackgroundImageUrl(), holder.ivPhoto);
-		//downLoadBgImageFromPrase(holder.ivPhoto,store.getBackgroundImageUrl());
-		
+		// downLoadBgImageFromPrase(holder.ivPhoto,store.getBackgroundImageUrl());
+
 		holder.tvName.setText(store.getName());
 		holder.tvSubType.setText(store.getSubType());
-		if(store.getTrophies()!=null && store.getTrophies().size()>0)
-		{
+		if (store.getTrophies() != null && store.getTrophies().size() > 0) {
 			holder.tvThrophy.setText(store.getTrophies().get(0));
 		}
-			
+
 		holder.btnMenu.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
-				Log.e("MSG>>>","btn menu clicked");
-				AsaanUtility.selectedStore=getItem(position);
-				Intent intent=new Intent(mContext,MenuActivity.class);
+				Log.e("MSG>>>", "btn menu clicked");
+				AsaanUtility.selectedStore = getItem(position);
+				Intent intent = new Intent(mContext, MenuActivity.class);
 				mContext.startActivity(intent);
-				
+
 			}
 		});
 		holder.btnorder.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Store store=getItem(position);
+				Store store = getItem(position);
 
-				int current=store.getId().intValue();
-				int savedId=AsaanUtility.getCurrentOrderedStoredId(mContext);
-				Log.e("size",store.getId().intValue()+" saved  "+savedId);
-				if(current==savedId)
-				{
-				AsaanUtility.selectedStore=getItem(position);
-				Intent intent=new Intent(mContext,MyCartActivity.class);
-				mContext.startActivity(intent);
-				}
-				else if(savedId==-1)
-				{
-					simpleAlert(mContext,"You have no orders.");
-				}
-				else
-					alert(mContext,"Already have saved order from other restaurant.Delete all orders?");
-				
-				
+				int current = store.getId().intValue();
+				int savedId = AsaanUtility.getCurrentOrderedStoredId(mContext);
+				Log.e("size", store.getId().intValue() + " saved  " + savedId);
+				if (current == savedId) {
+					AsaanUtility.selectedStore = getItem(position);
+					Intent intent = new Intent(mContext, MyCartActivity.class);
+					mContext.startActivity(intent);
+				} else if (savedId == -1) {
+					simpleAlert(mContext, "You have no orders.");
+				} else
+					alert(mContext, "Already have saved order from other restaurant.Delete all orders?");
+
 			}
 		});
 
 		return convertView;
 	}
-/*	private void downLoadBgImageFromPrase(final ParseImageView iv,String objectId)
-	{
-		ParseQuery<ParseObject> query=ParseQuery.getQuery("PictureFiles");
-		query.whereEqualTo("objectId",objectId);
-		query.findInBackground(new FindCallback<ParseObject>() {
-			
-			@Override
-			public void done(List<ParseObject> list, ParseException e) {
-				if(e==null && list.size()>0){
-					ParseObject obj=list.get(0);
-				 ParseFile file=obj.getParseFile("picture_file");
-				iv.setParseFile(file);
-				iv.loadInBackground();
-				
-				}
-				
-			}
-		});
-		
-	}*/
+
+	/*
+	 * private void downLoadBgImageFromPrase(final ParseImageView iv,String
+	 * objectId) { ParseQuery<ParseObject>
+	 * query=ParseQuery.getQuery("PictureFiles");
+	 * query.whereEqualTo("objectId",objectId); query.findInBackground(new
+	 * FindCallback<ParseObject>() {
+	 * 
+	 * @Override public void done(List<ParseObject> list, ParseException e) {
+	 * if(e==null && list.size()>0){ ParseObject obj=list.get(0); ParseFile
+	 * file=obj.getParseFile("picture_file"); iv.setParseFile(file);
+	 * iv.loadInBackground();
+	 * 
+	 * }
+	 * 
+	 * } });
+	 * 
+	 * }
+	 */
 	private void alert(final Context context, String message) {
-		AlertDialog.Builder bld = new AlertDialog.Builder(context,
-				AlertDialog.THEME_HOLO_LIGHT);
+		AlertDialog.Builder bld = new AlertDialog.Builder(context, AlertDialog.THEME_HOLO_LIGHT);
 		// bld.setTitle(context.getResources().getText(R.string.app_name));
 		bld.setTitle(R.string.app_name);
 		bld.setMessage(message);
@@ -187,17 +165,17 @@ public class StoreListAdapter extends ArrayAdapter<Store> {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				
-				//deleting all orders
+
+				// deleting all orders
 				OpenHelper helper = new DaoMaster.DevOpenHelper(mContext, "asaan-db", null);
-		        SQLiteDatabase db = helper.getWritableDatabase();
-		        DaoMaster  daoMaster = new DaoMaster(db);
-		        DaoSession daoSession = daoMaster.newSession();
-		        AddItemDao addItemDao = daoSession.getAddItemDao();
-				ModItemDao modItemDao=daoSession.getModItemDao();
+				SQLiteDatabase db = helper.getWritableDatabase();
+				DaoMaster daoMaster = new DaoMaster(db);
+				DaoSession daoSession = daoMaster.newSession();
+				AddItemDao addItemDao = daoSession.getAddItemDao();
+				ModItemDao modItemDao = daoSession.getModItemDao();
 				addItemDao.deleteAll();
 				modItemDao.deleteAll();
-				AsaanUtility.setCurrentOrderdStoreId(mContext,-1);
+				AsaanUtility.setCurrentOrderdStoreId(mContext, -1);
 				dialog.dismiss();
 			}
 		});
@@ -206,20 +184,20 @@ public class StoreListAdapter extends ArrayAdapter<Store> {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.dismiss();
-				
+
 			}
 		});
 
 		bld.create().show();
 	}
+
 	private void simpleAlert(final Context context, String message) {
-		AlertDialog.Builder bld = new AlertDialog.Builder(context,
-				AlertDialog.THEME_HOLO_LIGHT);
+		AlertDialog.Builder bld = new AlertDialog.Builder(context, AlertDialog.THEME_HOLO_LIGHT);
 		// bld.setTitle(context.getResources().getText(R.string.app_name));
 		bld.setTitle(R.string.app_name);
 		bld.setMessage(message);
 		bld.setCancelable(false);
-		bld.setNeutralButton("Ok",null);
+		bld.setNeutralButton("Ok", null);
 
 		bld.create().show();
 	}
--- src/com/techfiesta/asaan/fragment/InfoFragment.java
+++ src/com/techfiesta/asaan/fragment/InfoFragment.java
@@ -9,39 +9,37 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.MapFragment;
 import com.techfiesta.asaan.R;
-import com.techfiesta.asaan.activity.StoreDetailsActivity;
 import com.techfiesta.asaan.activity.StoreDetailsActivityNew;
 import com.techfiesta.asaan.utility.AsaanUtility;
 
 public class InfoFragment extends Fragment {
 
-	private TextView tvAddress, tvPhone,tvName;
+	private TextView tvAddress, tvPhone, tvName;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_info_2, container, false);
 		tvAddress = (TextView) v.findViewById(R.id.tv_address);
 		tvPhone = (TextView) v.findViewById(R.id.tv_phone);
-		tvName=(TextView)v.findViewById(R.id.tv_name);
+		tvName = (TextView) v.findViewById(R.id.tv_name);
 		tvAddress.setText(AsaanUtility.selectedStore.getAddress());
 		tvPhone.setText(AsaanUtility.selectedStore.getPhone());
 		tvName.setText(AsaanUtility.selectedStore.getName());
 
 		return v;
 	}
+
 	@Override
 	public void onDestroyView() {
-		if(!StoreDetailsActivityNew.isBackButtonPressed)
-		{
+		if (!StoreDetailsActivityNew.isBackButtonPressed) {
 			try {
-				MapFragment fragment = (MapFragment) getActivity()
-						.getFragmentManager().findFragmentById(
-								R.id.map);
-				if (fragment != null) getFragmentManager().beginTransaction().remove(fragment).commit();
+				MapFragment fragment = (MapFragment) getActivity().getFragmentManager().findFragmentById(R.id.map);
+				if (fragment != null)
+					getFragmentManager().beginTransaction().remove(fragment).commit();
 
 			} catch (IllegalStateException e) {
-				//handle this situation because you are necessary will get 
-				//an exception here :-(
+				// handle this situation because you are necessary will get
+				// an exception here :-(
 			}
 		}
 		super.onDestroyView();
