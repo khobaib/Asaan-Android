e3290cad5287407af904d532b59af0cc892cc04b
TanzeerH <hossaintanzeer@gmail.com>
2015-04-06 19:40:51 +0600

    discount option ui added

---
+++ src/com/techfiesta/asaan/activity/DiscountActivity.java
@@ -0,0 +1,41 @@
+package com.techfiesta.asaan.activity;
+
+import com.techfiesta.asaan.R;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DiscountActivity extends Activity{
+
+	private ActionBar actionBar;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_discount_code);
+		
+		actionBar=getActionBar();
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		if(item.getItemId()==android.R.id.home)
+		{
+			finish();
+			overridePendingTransition(R.anim.prev_slide_in, R.anim.prev_slide_out);
+		}
+		return true;
+	}
+}
